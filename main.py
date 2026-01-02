0643757f841229e009d717c9485687a8faf8e773
AdamElsving <adamheiseelswing@gmail.com>
2026-01-02 13:50:34 +0100

    Limited framrate to 60 fps

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pg.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -17,9 +20,13 @@ def main():
             if event.type == pg.QUIT:
                 return
 
+        for i in range (1, 1000000):
+            k = 5 / i
 
         screen.fill("black")
         pg.display.flip()
+
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
