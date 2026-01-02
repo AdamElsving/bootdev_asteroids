418d088d553ebca010dba1ec9e403037b0af6d83
AdamElsving <adamheiseelswing@gmail.com>
2026-01-02 15:16:09 +0100

    Added rotation controls to the player

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        dt = clock.tick(60) / 1000
+        
         log_state()
 
         for event in pygame.event.get():
@@ -25,10 +27,10 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
-        
+        player.update(dt)
+
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
